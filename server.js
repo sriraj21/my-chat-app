b2dfc68f3fe39f86eeef178b4646648381dacacb
sriraj21 <srirajr9@gmail.com>
2025-09-01 14:36:48 +0530

    CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://genuine-vacherin-f77495.netlify.app", // Your Netlify URL
+    origin: "https://genuine-vacherin-f77495.netlify.app",
     methods: ["GET", "POST"]
   }
 });
